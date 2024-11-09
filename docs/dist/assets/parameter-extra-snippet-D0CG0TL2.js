d71e2b90d8a619b89cd3d204963fd26db5f865b4
adairrr <32375605+adairrr@users.noreply.github.com>
2024-11-09 00:16:06 -0500

    Add topnav links

--- docs/dist/assets/parameter-extra-snippet-D0CG0TL2.js
+++
@@ -1,7 +0,0 @@
-import{u as i,j as n}from"./index-CD6P7Mgz.js";const r=void 0;function a(s){const e={a:"a",code:"code",div:"div",h3:"h3",p:"p",pre:"pre",span:"span",...i(),...s.components};return n.jsxs(n.Fragment,{children:[n.jsxs(e.h3,{id:"extra",children:["extra",n.jsx(e.a,{"aria-hidden":"true",tabIndex:"-1",href:"#extra",children:n.jsx(e.div,{"data-autolink-icon":!0})})]}),`
-`,n.jsx(e.pre,{className:"shiki shiki-themes github-light github-dark-dimmed",style:{backgroundColor:"#fff","--shiki-dark-bg":"#22272e",color:"#24292e","--shiki-dark":"#adbac7"},tabIndex:"0",children:n.jsxs(e.code,{children:[n.jsx(e.span,{className:"line",children:n.jsx(e.span,{children:"{"})}),`
-`,n.jsx(e.span,{className:"line",children:n.jsx(e.span,{children:"    apiUrl?: string | undefined;"})}),`
-`,n.jsx(e.span,{className:"line",children:n.jsx(e.span,{children:"    accountId?: AccountId | undefined;"})}),`
-`,n.jsx(e.span,{className:"line",children:n.jsx(e.span,{children:"    cosmWasmClient?: CosmWasmClient | undefined;"})}),`
-`,n.jsx(e.span,{className:"line",children:n.jsx(e.span,{children:"} | undefined"})})]})}),`
-`,n.jsx(e.p,{children:"Extra options to override decorated parameters."})]})}function t(s={}){const{wrapper:e}={...i(),...s.components};return e?n.jsx(e,{...s,children:n.jsx(a,{...s})}):a(s)}export{t as default,r as frontmatter};
--- vocs.config.ts
+++ vocs.config.ts
@@ -2,7 +2,28 @@ import { defineConfig } from 'vocs'
 
 export default defineConfig({
   title: 'Docs',
-  logoUrl: { dark: '/logo-dark.svg', light: '/logo-light.svg' },
+  logoUrl: {dark: '/logo-dark.svg', light: '/logo-light.svg'},
+  socials: [
+    {
+      icon: 'github',
+      link: 'https://github.com/AbstractSDK/abstract.js',
+    },
+    {
+      icon: 'discord',
+      link: 'http://discord.gg/uch3Tq3aym',
+    },
+    {
+      icon: 'x',
+      link: 'https://x.com/AbstractSDK',
+    },
+  ],
+  topNav: [
+    {text: 'Docs', link: '/getting-started', match: '/docs'},
+    {
+      text: 'Examples',
+      link: 'https://github.com/AbstractSDK/abstract.js/tree/mainline/examples',
+    },
+  ],
   sidebar: [
     {
       text: 'Introduction',
@@ -19,7 +40,7 @@ export default defineConfig({
           text: 'Introduction',
           link: '/cli/introduction',
         },
-      ]
+      ],
     },
     {
       text: 'Core',
@@ -28,221 +49,231 @@ export default defineConfig({
           text: 'Introduction',
           link: '/core/introduction',
         },
-      ]
+      ],
     },
     {
       text: 'React',
       items: [
         {
           text: 'Provider Examples',
-          items: [{
-            text: 'Graz',
-            link: '/react/providers/graz',
-          }, {
-            text: 'Xion',
-            link: '/react/providers/xion',
-          }],
+          items: [
+            {
+              text: 'Graz',
+              link: '/react/providers/graz',
+            },
+            {
+              text: 'Xion',
+              link: '/react/providers/xion',
+            },
+          ],
         },
-      ]
+      ],
     },
     {
       text: 'Hooks',
-      items: [{
-        text: 'Public',
-        items: [
-          {
-
-            text: 'useAbstractModuleVersion',
-            link: '/hooks/public/use-abstract-module-version',
-          },
-          {
-            text: 'useAccountsBaseAddressesFromApi',
-            link: '/hooks/public/use-accounts-base-addresses-from-api',
-          },
-          {
-            text: 'useCosmWasmClient',
-            link: '/hooks/public/use-cosm-wasm-client',
-          },
-          {
-            text: 'useRemoteHosts',
-            link: '/hooks/public/use-remote-hosts',
-          },
-          {
-            text: 'useSimulateRemoteMsg',
-            link: '/hooks/public/use-simulate-remote-msg',
-          },
-          {
-            text: 'useSmartQuery',
-            link: '/hooks/public/use-smart-query',
-          },
-        ]
-      },
-      {
-        text: 'Wallet',
-        items: [{
-          text: 'Utils',
-            items: [
-              {
-                text: 'useSignAndBroadcast',
-                link: '/hooks/wallet/utils/use-sign-and-broadcast',
-              },
-            ]
-          },
-          {
-            text: 'useCreateAccountMonarchy',
-            link: '/hooks/wallet/use-create-account-monarchy',
-          },
-          {
-            text: 'useCreateAccount',
-            link: '/hooks/wallet/use-create-account',
-          },
-          {
-            text: 'useSenderAddress',
-            link: '/hooks/wallet/use-sender-address',
-          },
-          {
-            text: 'useSigningCosmWasmClient',
-            link: '/hooks/wallet/use-signing-cosm-wasm-client',
-          },
-        ]
-      },
-      {
-        text: 'Account',
-        items: [
-          {
-            text: 'Public', items: [
-              {
-                text: 'useAccountAddressFromApi',
-                link: '/hooks/account/public/use-account-address-from-api',
-              },
-              {
-                text: 'useAccountInstantiate2Address',
-                link: '/hooks/account/public/use-account-instantiate2-address',
-              },
-              {
-                text: 'useAccountSettings',
-                link: '/hooks/account/public/use-account-settings',
-              },
-              {
-                text: 'useModuleInstantiate2Address',
-                link: '/hooks/account/public/use-module-instantiate2-address',
-              },
-              {
-                text: 'useModules',
-                link: '/hooks/account/public/use-modules',
-              },
-              {
-                text: 'useRemoteAccountIds',
-                link: '/hooks/account/public/use-remote-account-ids',
-              },
-              {
-                text: 'useRemoteAccounts',
-                link: '/hooks/account/public/use-remote-accounts',
-              },
-              {
-                text: 'useSimulateExecuteRemoteAccount',
-                link: '/hooks/account/public/use-simulate-execute-remote-account',
-              },
-              {
-                text: 'useSubAccountIdsFromApi',
-                link: '/hooks/account/public/use-sub-account-ids-from-api',
-              },
-            ]
-          },
-          {
-            text: 'Wallet', items: [
-              {
-                text: 'useCreateRemoteAccount',
-                link: '/hooks/account/wallet/use-create-remote-account',
-              },
-              {
-                text: 'useCreateSubAccount',
-                link: '/hooks/account/wallet/use-create-sub-account',
-              },
-              {
-                text: 'useDeposit',
-                link: '/hooks/account/wallet/use-deposit',
-              },
-              {
-                text: 'useEnableIbc',
-                link: '/hooks/account/wallet/use-enable-ibc',
-              },
-              {
-                text: 'useExecuteOnRemoteModule',
-                link: '/hooks/account/wallet/use-execute-on-remote-module',
-              },
-              {
-                text: 'useExecuteOnRemote',
-                link: '/hooks/account/wallet/use-execute-on-remote',
-              },
-              {
-                text: 'useExecute',
-                link: '/hooks/account/wallet/use-execute',
-              },
-              {
-                text: 'useRequestRemoteFunds',
-                link: '/hooks/account/wallet/use-request-remote-funds',
-              },
-              {
-                text: 'useSendFundsToRemote',
-                link: '/hooks/account/wallet/use-send-funds-to-remote',
-              },
-              {
-                text: 'useUpdateInfo',
-                link: '/hooks/account/wallet/use-update-info',
-              },
-              {
-                text: 'useUpdateSettings',
-                link: '/hooks/account/wallet/use-update-settings',
-              },
-              {
-                text: 'useUpgradeModule',
-                link: '/hooks/account/wallet/use-upgrade-module',
-              },
-              {
-                text: 'useWithdraw',
-                link: '/hooks/account/wallet/use-withdraw',
-              },
-            ]
-          }
-        ]
-      },
-      {
-        text: 'useAccountBalancesFromApi',
-        link: '/hooks/use-account-balances-from-api',
-      },
-      {
-        text: 'useAccounts',
-        link: '/hooks/use-accounts',
-      },
-      {
-        text: 'useAnsTokenFromApi',
-        link: '/hooks/use-ans-token-from-api',
-      },
-      {
-        text: 'useModulesFromApi',
-        link: '/hooks/use-modules-from-api',
-      },
-      {
-        text: 'useSubAccountIdsByModuleIdFromApi',
-        link: '/hooks/use-sub-account-ids-by-module-id-from-api',
-      },
-      ]
+      items: [
+        {
+          text: 'Public',
+          items: [
+            {
+              text: 'useAbstractModuleVersion',
+              link: '/hooks/public/use-abstract-module-version',
+            },
+            {
+              text: 'useAccountsBaseAddressesFromApi',
+              link: '/hooks/public/use-accounts-base-addresses-from-api',
+            },
+            {
+              text: 'useCosmWasmClient',
+              link: '/hooks/public/use-cosm-wasm-client',
+            },
+            {
+              text: 'useRemoteHosts',
+              link: '/hooks/public/use-remote-hosts',
+            },
+            {
+              text: 'useSimulateRemoteMsg',
+              link: '/hooks/public/use-simulate-remote-msg',
+            },
+            {
+              text: 'useSmartQuery',
+              link: '/hooks/public/use-smart-query',
+            },
+          ],
+        },
+        {
+          text: 'Wallet',
+          items: [
+            {
+              text: 'Utils',
+              items: [
+                {
+                  text: 'useSignAndBroadcast',
+                  link: '/hooks/wallet/utils/use-sign-and-broadcast',
+                },
+              ],
+            },
+            {
+              text: 'useCreateAccountMonarchy',
+              link: '/hooks/wallet/use-create-account-monarchy',
+            },
+            {
+              text: 'useCreateAccount',
+              link: '/hooks/wallet/use-create-account',
+            },
+            {
+              text: 'useSenderAddress',
+              link: '/hooks/wallet/use-sender-address',
+            },
+            {
+              text: 'useSigningCosmWasmClient',
+              link: '/hooks/wallet/use-signing-cosm-wasm-client',
+            },
+          ],
+        },
+        {
+          text: 'Account',
+          items: [
+            {
+              text: 'Public',
+              items: [
+                {
+                  text: 'useAccountAddressFromApi',
+                  link: '/hooks/account/public/use-account-address-from-api',
+                },
+                {
+                  text: 'useAccountInstantiate2Address',
+                  link: '/hooks/account/public/use-account-instantiate2-address',
+                },
+                {
+                  text: 'useAccountSettings',
+                  link: '/hooks/account/public/use-account-settings',
+                },
+                {
+                  text: 'useModuleInstantiate2Address',
+                  link: '/hooks/account/public/use-module-instantiate2-address',
+                },
+                {
+                  text: 'useModules',
+                  link: '/hooks/account/public/use-modules',
+                },
+                {
+                  text: 'useRemoteAccountIds',
+                  link: '/hooks/account/public/use-remote-account-ids',
+                },
+                {
+                  text: 'useRemoteAccounts',
+                  link: '/hooks/account/public/use-remote-accounts',
+                },
+                {
+                  text: 'useSimulateExecuteRemoteAccount',
+                  link: '/hooks/account/public/use-simulate-execute-remote-account',
+                },
+                {
+                  text: 'useSubAccountIdsFromApi',
+                  link: '/hooks/account/public/use-sub-account-ids-from-api',
+                },
+              ],
+            },
+            {
+              text: 'Wallet',
+              items: [
+                {
+                  text: 'useCreateRemoteAccount',
+                  link: '/hooks/account/wallet/use-create-remote-account',
+                },
+                {
+                  text: 'useCreateSubAccount',
+                  link: '/hooks/account/wallet/use-create-sub-account',
+                },
+                {
+                  text: 'useDeposit',
+                  link: '/hooks/account/wallet/use-deposit',
+                },
+                {
+                  text: 'useEnableIbc',
+                  link: '/hooks/account/wallet/use-enable-ibc',
+                },
+                {
+                  text: 'useExecuteOnRemoteModule',
+                  link: '/hooks/account/wallet/use-execute-on-remote-module',
+                },
+                {
+                  text: 'useExecuteOnRemote',
+                  link: '/hooks/account/wallet/use-execute-on-remote',
+                },
+                {
+                  text: 'useExecute',
+                  link: '/hooks/account/wallet/use-execute',
+                },
+                {
+                  text: 'useRequestRemoteFunds',
+                  link: '/hooks/account/wallet/use-request-remote-funds',
+                },
+                {
+                  text: 'useSendFundsToRemote',
+                  link: '/hooks/account/wallet/use-send-funds-to-remote',
+                },
+                {
+                  text: 'useUpdateInfo',
+                  link: '/hooks/account/wallet/use-update-info',
+                },
+                {
+                  text: 'useUpdateSettings',
+                  link: '/hooks/account/wallet/use-update-settings',
+                },
+                {
+                  text: 'useUpgradeModule',
+                  link: '/hooks/account/wallet/use-upgrade-module',
+                },
+                {
+                  text: 'useWithdraw',
+                  link: '/hooks/account/wallet/use-withdraw',
+                },
+              ],
+            },
+          ],
+        },
+        {
+          text: 'useAccountBalancesFromApi',
+          link: '/hooks/use-account-balances-from-api',
+        },
+        {
+          text: 'useAccounts',
+          link: '/hooks/use-accounts',
+        },
+        {
+          text: 'useAnsTokenFromApi',
+          link: '/hooks/use-ans-token-from-api',
+        },
+        {
+          text: 'useModulesFromApi',
+          link: '/hooks/use-modules-from-api',
+        },
+        {
+          text: 'useSubAccountIdsByModuleIdFromApi',
+          link: '/hooks/use-sub-account-ids-by-module-id-from-api',
+        },
+      ],
     },
-
     {
       text: 'Miscellaneous',
       items: [
         {
           text: 'Running Abstract API',
           link: '/miscellaneous/running-abstract-api',
-        }
-      ]
+        },
+      ],
     },
   ],
+  editLink: {
+    pattern:
+      'https://github.com/AbstractSDK/abstract.js-docs/blob/main/docs/pages/:path',
+    text: 'Edit this page on GitHub',
+  },
   twoslash: {
     compilerOptions: {
-      jsx: 1
-    }
+      jsx: 1,
+    },
   },
 })
