f10d7fa216fbb8886ecc1e7fed3392a2b7baac82
adairrr <32375605+adairrr@users.noreply.github.com>
2024-12-12 10:59:38 -0500

    Update xion link

---
+++ docs/dist/assets/parameter-extra-snippet-csoT1hDy.js
@@ -0,0 +1,8 @@
+import{u as a,j as n}from"./index-BY5WT8jg.js";const d=void 0;function i(s){const e={a:"a",code:"code",div:"div",h3:"h3",p:"p",pre:"pre",span:"span",...a(),...s.components};return n.jsxs(n.Fragment,{children:[n.jsxs(e.h3,{id:"extra",children:["extra",n.jsx(e.a,{"aria-hidden":"true",tabIndex:"-1",href:"#extra",children:n.jsx(e.div,{"data-autolink-icon":!0})})]}),`
+`,n.jsx(e.pre,{className:"shiki shiki-themes github-light github-dark-dimmed",style:{backgroundColor:"#fff","--shiki-dark-bg":"#22272e",color:"#24292e","--shiki-dark":"#adbac7"},tabIndex:"0",children:n.jsxs(e.code,{children:[n.jsx(e.span,{className:"line",children:n.jsx(e.span,{style:{color:"#24292E","--shiki-dark":"#ADBAC7"},children:"{"})}),`
+`,n.jsx(e.span,{className:"line",children:n.jsx(e.span,{style:{color:"#24292E","--shiki-dark":"#ADBAC7"},children:"    apiUrl?: string | undefined;"})}),`
+`,n.jsx(e.span,{className:"line",children:n.jsx(e.span,{style:{color:"#24292E","--shiki-dark":"#ADBAC7"},children:"    accountId?: AccountId | undefined;"})}),`
+`,n.jsx(e.span,{className:"line",children:n.jsx(e.span,{style:{color:"#24292E","--shiki-dark":"#ADBAC7"},children:"    signingCosmWasmClient?: SigningCosmWasmClient | undefined;"})}),`
+`,n.jsx(e.span,{className:"line",children:n.jsx(e.span,{style:{color:"#24292E","--shiki-dark":"#ADBAC7"},children:"    sender: string | undefined;"})}),`
+`,n.jsx(e.span,{className:"line",children:n.jsx(e.span,{style:{color:"#24292E","--shiki-dark":"#ADBAC7"},children:"} | undefined"})})]})}),`
+`,n.jsx(e.p,{children:"Extra options to override decorated parameters."})]})}function t(s={}){const{wrapper:e}={...a(),...s.components};return e?n.jsx(e,{...s,children:n.jsx(i,{...s})}):i(s)}export{t as default,d as frontmatter};
